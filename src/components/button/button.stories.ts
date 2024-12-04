c8ab738cc11872377cae3c588d0e095ee6ba3f15
Daiane Oliveira <daianeop3@gmail.com>
2024-12-04 18:38:46 -0300

    add prettier changes

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button } from "./button"
+import { Button } from './button'
 
 const meta = {
   title: 'Components/Button',
@@ -11,19 +11,19 @@ const meta = {
   tags: ['autodocs'],
   argTypes: {},
   args: {},
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Button>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
     children: 'Primary Button',
   },
-};
+}
 
 export const Secondary: Story = {
   args: {
     children: 'Secondary Button',
   },
-};
+}
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, FC } from "react"
-import styles from "./button.module.scss"
+import { ReactNode, FC } from 'react'
+import styles from './button.module.scss'
 
-export const Button: FC<{children: ReactNode}> = ({children}) => {
-    return <button className={styles.btn}>{children}</button>
+export const Button: FC<{ children: ReactNode }> = ({ children }) => {
+  return <button className={styles.btn}>{children}</button>
 }
